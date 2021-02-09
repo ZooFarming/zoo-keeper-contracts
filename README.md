860664e76f65f762d184f5289cfe7110cfc81f39
lolieatapple <lolieatapple@gmail.com>
2021-02-09 10:40:22 +0800

    first add

---
+++ README.md
@@ -0,0 +1,8 @@
+# ZOO KEEPER CONTRACTS
+
+ZooKeeper mainly includes four contracts, Token, Farming, Boosting and NFT.
+
+Among them, Token and Farming are non-upgradable contracts to guarantee the security of the user's principal.
+
+Boosting and NFT are upgradable contracts, which facilitate the future expansion of richer gameplay.
+
---
+++ contracts/token/ZooToken.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.6.12;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+// WaspToken
+contract ZooToken is ERC20("ZooToken", "ZOO"), Ownable {
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (WanSwapFarm).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}

6f30c315b22e02b413f588f5e2208e4b7da3c00b
lolieatapple <lolieatapple@gmail.com>
2021-02-12 19:05:39 +0800

    NFT finish

--- contracts/NFT/ZooNFTDelegate.sol
+++ contracts/NFT/ZooNFTDelegate.sol
@@ -17,5 +17,47 @@ contract ZooNFT is ERC721("ZooNFT", "ZooNFT"), Initializable, AccessControl, Zoo
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
+    function setNFTFactory(address _nftFactory) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        _setupRole(NFT_FACTORY_ROLE, _nftFactory);
+    }
+
+    function setBaseURI(string memory _baseURI) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        _setBaseURI(_baseURI);
+    }
+
+    // Use for boosting calc: boosting = (level - 1) * a + category * b + item * c + random * d;
+    function setScaleParams(uint _a, uint _b, uint _c, uint _d) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        scaleParams.a = _a;
+        scaleParams.b = _b;
+        scaleParams.c = _c;
+        scaleParams.d = _d;
+    }
+
+    // Use for get boosting: boosting = (level - 1) * a + category * b + item * c + random * d;
+    function getBoosting(uint _tokenId) external view returns (uint) {
+        return tokenInfo[_tokenId].level.sub(1).mul(scaleParams.a) + tokenInfo[_tokenId].category.mul(scaleParams.b) + tokenInfo[_tokenId].item.mul(scaleParams.c) + tokenInfo[_tokenId].random.mul(scaleParams.d);
+    }
     
+    function mint(uint tokenId, uint _level, uint _category, uint _item, uint _random) external {
+        require(hasRole(NFT_FACTORY_ROLE, msg.sender));
+        _safeMint(msg.sender, tokenId);
+        require(_level > 0 && _level < 5, "level must larger than 0, lesser than 5");
+        tokenInfo[tokenId].level = _level;
+        tokenInfo[tokenId].category = _category;
+        tokenInfo[tokenId].item = _item;
+        tokenInfo[tokenId].random = _random;
+    }
+
+    function foundationMint(uint tokenId, uint _level, uint _category, uint _item, uint _random) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        _safeMint(msg.sender, tokenId);
+        require(_level > 0 && _level < 5, "level must larger than 0, lesser than 5");
+        tokenInfo[tokenId].level = _level;
+        tokenInfo[tokenId].category = _category;
+        tokenInfo[tokenId].item = _item;
+        tokenInfo[tokenId].random = _random;
+    }
 }
--- contracts/NFT/ZooNFTStorage.sol
+++ contracts/NFT/ZooNFTStorage.sol
@@ -4,4 +4,23 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract ZooNFTStorage {
     using SafeMath for uint256;
+
+    struct TokenInfo {
+        uint256 level;
+        uint256 category;
+        uint256 item;
+        uint256 random;
+    }
+
+    // Use for boosting calc: boosting = (level - 1) * a + category * b + item * c + random * d;
+    struct ScaleParams {
+        uint256 a;
+        uint256 b;
+        uint256 c;
+        uint256 d;
+    }
+
+    ScaleParams public scaleParams;
+
+    mapping(uint => TokenInfo) public tokenInfo;
 }

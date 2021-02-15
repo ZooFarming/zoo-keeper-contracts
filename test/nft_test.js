6269e60ce5bf231ff991f66df6f380b93d644f3c
lolieatapple <lolieatapple@gmail.com>
2021-02-16 00:25:25 +0800

    update test

--- test/nft_test.js
+++ test/nft_test.js
@@ -1,9 +1,13 @@
 const ZooNFTDelegate = artifacts.require('ZooNFTDelegate');
+
 const assert = require('assert');
 
 contract("ZooNFTDelegate", accounts => {
-  it("all", async () => {
+  it.only("all", async () => {
     const zooNFTDelegate = await ZooNFTDelegate.new(accounts[0]);
-
+    await zooNFTDelegate.initialize(accounts[0]);
+    await zooNFTDelegate.setNftURI(1, 1, 1, 'https://gateway.pinata.cloud/ipfs/QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    let ret = await zooNFTDelegate.getNftURI(1,1,1);
+    console.log('ret', ret);
   });
 });

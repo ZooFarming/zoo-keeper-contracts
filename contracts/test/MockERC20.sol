11262c69f3f18c029f170f28c2c686019b64ca6f
lolieatapple <lolieatapple@gmail.com>
2021-02-17 23:13:41 +0800

    update test

---
+++ contracts/test/MockERC20.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// WaspToken
+contract MockERC20 is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 _decimals,
+        uint256 mintAmount
+    ) public ERC20(name, symbol) {
+        _setupDecimals(_decimals);
+        _mint(msg.sender, mintAmount);
+    }
+}

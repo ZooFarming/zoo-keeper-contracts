3e7fc4f62fa60ec1febf34cce387b2d22edbdceb
lolieatapple <lolieatapple@gmail.com>
2021-02-25 21:17:47 +0800

    all unit test finish

--- test/farming.test.js
+++ test/farming.test.js
@@ -388,239 +388,173 @@ contract('ZooKeeperFarming', ([alice, bob, carol, dev, minter]) => {
     assert.strictEqual((await lp1.balanceOf(bob)).toString(), '1000000');
   });
 
-  return;
-  console.log("Please remember to restart ganache-cli before each test.")
-
-  beforeEach(async () => {
-    this.zoo = await ZooToken.new({ from: alice });
+  it("should success when withdraw amount no-lock to lock 1", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 100, 0, 0, {from: bob});
+    await farming.deposit(0, 100, 3600*24*30, 0, {from: bob});
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, bob);
+    assert.strictEqual(ret.toString(), '11');
+    ret = await boosting.userInfo(0, bob);
+    try { 
+      await farming.withdraw(0, 100, {from: bob});
+      assert.fail('never go here');
+    } catch (e) {
+      assert.ok(e.message.match(/revert/));
+    }
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, bob);
+    assert.strictEqual(ret.toString(), '33');
   });
 
-  it('should set correct state variables', async () => {
-    this.farm = await ZooKeeperFarming.new(this.zoo.address, dev,
-      ZERO_ADDRESS,
-      '10000000000000000000',
-      0,
-      99999999,
-      ZERO_ADDRESS,
-      ZERO_ADDRESS, { from: alice });
-    await this.zoo.transferOwnership(this.farm.address, { from: alice });
-    const zoo = await this.farm.zoo();
-    const devaddr = await this.farm.devaddr();
-    const owner = await this.zoo.owner();
-    assert.equal(zoo.valueOf(), this.zoo.address);
-    assert.equal(devaddr.valueOf(), dev);
-    assert.equal(owner.valueOf(), this.farm.address);
+  it("should success when withdraw amount no-lock to lock 2", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 100, 0, 0, {from: bob});
+    await farming.deposit(0, 100, 3, 0, {from: bob});
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, bob);
+    assert.strictEqual(ret.toString(), '10');
+    await sleep(5000);
+    await time.advanceBlock();
+    await farming.withdraw(0, 200, {from: bob});
+    assert.strictEqual((await lp1.balanceOf(bob)).toString(), '1000000');
   });
 
-  it('should allow dev and only dev to update dev', async () => {
-    this.farm = await ZooKeeperFarming.new(this.zoo.address, dev,
-      ZERO_ADDRESS,
-      '10000000000000000000',
-      0,
-      99999999,
-      ZERO_ADDRESS,
-      ZERO_ADDRESS, { from: alice });
-    assert.equal((await this.farm.devaddr()).valueOf(), dev);
-    await expectRevert(this.farm.dev(bob, { from: bob }), 'Should be dev address');
-    await this.farm.dev(bob, { from: dev });
-    assert.equal((await this.farm.devaddr()).valueOf(), bob);
-    await this.farm.dev(alice, { from: bob });
-    assert.equal((await this.farm.devaddr()).valueOf(), alice);
-  })
-
-  context('With ERC/LP token added to the field', () => {
-    beforeEach(async () => {
-      this.lp = await MockERC20.new('LPToken', 'LP', 18, '10000000000', { from: minter });
-      await this.lp.transfer(alice, '1000', { from: minter });
-      await this.lp.transfer(bob, '1000', { from: minter });
-      await this.lp.transfer(carol, '1000', { from: minter });
-      this.lp2 = await MockERC20.new('LPToken2', 'LP2', 18, '10000000000', { from: minter });
-      await this.lp2.transfer(alice, '1000', { from: minter });
-      await this.lp2.transfer(bob, '1000', { from: minter });
-      await this.lp2.transfer(carol, '1000', { from: minter });
-
-      this.lp3 = await MockERC20.new('LPToken3', 'LP3', 4, '10000000000', { from: minter });
-      await this.lp3.transfer(alice, '1000', { from: minter });
-      await this.lp3.transfer(bob, '1000', { from: minter });
-      await this.lp3.transfer(carol, '1000', { from: minter });
-
-      this.lp4 = await MockERC20.new('LPToken4', 'LP4', 8, '10000000000', { from: minter });
-      await this.lp4.transfer(alice, '1000', { from: minter });
-      await this.lp4.transfer(bob, '1000', { from: minter });
-      await this.lp4.transfer(carol, '1000', { from: minter });
-    });
-
-    it('should give out ZOOs only after farming time', async () => {
-      // 100 per block farming rate starting at block 100 with bonus until block 1000
-      this.farm = await ZooKeeperFarming.new(this.zoo.address, dev,
-        ZERO_ADDRESS,
-        '100',
-        100,
-        99999999,
-        ZERO_ADDRESS,
-        ZERO_ADDRESS, { from: alice });
-      await this.zoo.transferOwnership(this.farm.address, { from: alice });
-      await this.farm.add('100', this.lp.address, true, 0, false);
-      await this.farm.set(0, '50', true);
-      await this.farm.set(0, '100', false);
-      await this.lp.approve(this.farm.address, '1000', { from: bob });
-      await this.farm.deposit(0, '100', 0, 0, { from: bob });
-      await time.advanceBlockTo('89');
-      await this.farm.withdraw(0, '0', { from: bob }); // block 90
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '0');
-      await time.advanceBlockTo('94');
-      await this.farm.withdraw(0, '0', { from: bob }); // block 95
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '0');
-      await time.advanceBlockTo('99');
-      await this.farm.withdraw(0, '0', { from: bob }); // block 100
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '0');
-      await time.advanceBlockTo('100');
-      await this.farm.withdraw(0, '0', { from: bob }); // block 101
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '100');
-      await time.advanceBlockTo('104');
-      await this.farm.withdraw(0, '0', { from: bob }); // block 105
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '500');
-      assert.equal((await this.zoo.balanceOf(dev)).valueOf(), '140');
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '640');
-    });
-
-    it('should not distribute ZOOs if no one deposit', async () => {
-      // 100 per block farming rate starting at block 200 with bonus until block 1000
-      this.farm = await ZooKeeperFarming.new(this.zoo.address, dev,
-        ZERO_ADDRESS,
-        '100',
-        200,
-        99999999,
-        ZERO_ADDRESS,
-        ZERO_ADDRESS, { from: alice });
-      await this.zoo.transferOwnership(this.farm.address, { from: alice });
-      await this.farm.add('100', this.lp.address, true, 0, false);
-      await this.lp.approve(this.farm.address, '1000', { from: bob });
-      await time.advanceBlockTo('199');
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '0');
-      await time.advanceBlockTo('204');
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '0');
-      await time.advanceBlockTo('209');
-      await this.farm.deposit(0, '10', 0, 0, { from: bob }); // block 210
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(dev)).valueOf(), '0');
-      assert.equal((await this.lp.balanceOf(bob)).valueOf(), '990');
-      await time.advanceBlockTo('219');
-      await this.farm.withdraw(0, '10', { from: bob }); // block 220
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '1280');
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '1000');
-      assert.equal((await this.zoo.balanceOf(dev)).valueOf(), '280');
-      assert.equal((await this.lp.balanceOf(bob)).valueOf(), '1000');
-    });
-
-    it('should distribute ZOOs properly for each staker', async () => {
-      // 100 per block farming rate starting at block 300 with bonus until block 1000
-      this.farm = await ZooKeeperFarming.new(this.zoo.address, dev,
-        ZERO_ADDRESS,
-        '100',
-        300,
-        99999999,
-        ZERO_ADDRESS,
-        ZERO_ADDRESS, { from: alice });
-      await this.zoo.transferOwnership(this.farm.address, { from: alice });
-      await this.farm.add('100', this.lp.address, true, 0, false);
-      await this.lp.approve(this.farm.address, '1000', { from: alice });
-      await this.lp.approve(this.farm.address, '1000', { from: bob });
-      await this.lp.approve(this.farm.address, '1000', { from: carol });
-      // Alice deposits 10 LPs at block 310
-      await time.advanceBlockTo('309');
-      await this.farm.deposit(0, '10', 0, 0, { from: alice });
-      await time.advanceBlockTo('311');
-      await time.advanceBlockTo('312');
-      // Bob deposits 20 LPs at block 314
-      await time.advanceBlockTo('313');
-      await this.farm.deposit(0, '20', 0, 0, { from: bob });
-
-      // Carol deposits 30 LPs at block 318
-      await time.advanceBlockTo('317');
-      await this.farm.deposit(0, '30', 0, 0, { from: carol });
-
-      // Alice deposits 10 more LPs at block 320. At this point:
-      //   Alice should have: 4*100 + 4*1/3*100 + 2*1/6*100 = 566
-      await time.advanceBlockTo('319')
-      await this.farm.deposit(0, '10', 0, 0, { from: alice });
-
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '724');
-      assert.equal((await this.zoo.balanceOf(alice)).valueOf(), '566');
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(carol)).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(this.farm.address)).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(dev)).valueOf(), '158');
-      // Bob withdraws 5 LPs at block 330. At this point:
-      //   Bob should have: 4*2/3*100 + 2*2/6*100 + 10*2/7*100 = 619
-      await time.advanceBlockTo('329')
-      await this.farm.withdraw(0, '5', { from: bob });
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '1516');
-      assert.equal((await this.zoo.balanceOf(alice)).valueOf(), '566');
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '619');
-      assert.equal((await this.zoo.balanceOf(carol)).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(this.farm.address)).valueOf(), '0');
-      assert.equal((await this.zoo.balanceOf(dev)).valueOf(), '331');
-      // Alice withdraws 20 LPs at block 340.
-      // Bob withdraws 15 LPs at block 350.
-      // Carol withdraws 30 LPs at block 360.
-      await time.advanceBlockTo('339')
-      await this.farm.withdraw(0, '20', { from: alice });
-      await time.advanceBlockTo('349')
-      await this.farm.withdraw(0, '15', { from: bob });
-      await time.advanceBlockTo('359')
-      await this.farm.withdraw(0, '30', { from: carol });
-      assert.equal((await this.zoo.totalSupply()).valueOf(), '6396');
-      assert.equal((await this.zoo.balanceOf(dev)).valueOf(), '1397');
-      // Alice should have: 566 + 10*2/7*100 + 10*2/6.5*100 = 1160
-      assert.equal((await this.zoo.balanceOf(alice)).valueOf(), '1159');
-      // Bob should have: 619 + 10*1.5/6.5*100 + 10*1.5/4.5*100 = 1183
-      assert.equal((await this.zoo.balanceOf(bob)).valueOf(), '1183');
-      // Carol should have: 2*3/6*100 + 10*3/7*100 + 10*3/6.5*100 + 10*3/4.5*100 + 10*100 = 2657
-      assert.equal((await this.zoo.balanceOf(carol)).valueOf(), '2657');
-      // All of them should have 1000 LPs back.
-      assert.equal((await this.lp.balanceOf(alice)).valueOf(), '1000');
-      assert.equal((await this.lp.balanceOf(bob)).valueOf(), '1000');
-      assert.equal((await this.lp.balanceOf(carol)).valueOf(), '1000');
-
-      let poolLength = await this.farm.poolLength();
-      console.log('poolLength', poolLength);
-
-    });
-
-    it('should give proper ZOO allocation to each pool', async () => {
-      // 100 per block farming rate starting at block 400 with bonus until block 1000
-      this.farm = await ZooKeeperFarming.new(this.zoo.address, dev,
-        ZERO_ADDRESS,
-        '100',
-        400,
-        1000,
-        ZERO_ADDRESS,
-        ZERO_ADDRESS, { from: alice });
-      await this.zoo.transferOwnership(this.farm.address, { from: alice });
-      await this.lp.approve(this.farm.address, '1000', { from: alice });
-      await this.lp2.approve(this.farm.address, '1000', { from: bob });
-      // Add first LP to the pool with allocation 1
-      await this.farm.add('10', this.lp.address, true, 0, false);
-      // Alice deposits 10 LPs at block 410
-      await time.advanceBlockTo('409');
-      await this.farm.deposit(0, '10', 0, 0, { from: alice });
-      // Add LP2 to the pool with allocation 2 at block 420
-      await time.advanceBlockTo('419');
-      await this.farm.add('20', this.lp2.address, true, 0, false);
-      // Alice should have 10*100 pending reward
-      assert.equal((await this.farm.pendingZoo(0, alice)).valueOf(), '1000');
-      // Bob deposits 10 LP2s at block 425
-      await time.advanceBlockTo('424');
-      await this.farm.deposit(1, '5', 0, 0, { from: bob });
-      // Alice should have 1000 + 5*1/3*100 = 1167 pending reward
-      assert.equal((await this.farm.pendingZoo(0, alice)).valueOf(), '1166');
-      await time.advanceBlockTo('430');
-      // At block 430. Bob should get 5*2/3*100 = 333.
-      // At block 430. Alice should get 1166 + 5*1/3*100 = 1333.
-      assert.equal((await this.farm.pendingZoo(0, alice)).valueOf(), '1333');
-      assert.equal((await this.farm.pendingZoo(1, bob)).valueOf(), '333');
-    });
+  it("should success when deposit 0 with NFT", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 0, 0, 1, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '2');
   });
+
+  it("should success when deposit amount with NFT", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 1000, 0, 1, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+  });
+
+  it("should success when deposit 0 no nft to nft", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 1000, 0, 0, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '2');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '10');
+    await farming.deposit(0, 0, 0, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '11');
+  });
+
+  it("should success when deposit amount no nft to nft", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 1000, 0, 0, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '2');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '10');
+    await farming.deposit(0, 1000, 0, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '11');
+  });
+
+  it("should success when withdraw 0 with nft", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    await farming.deposit(0, 1000, 0, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '11');
+    await time.advanceBlock();
+    await farming.withdraw(0, 0, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '0');
+  });
+
+  it("should success when withdraw 0 with nft", async ()=>{
+    await farming.add(100, lp1.address, true, 0, false);
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+    await farming.deposit(0, 1000, 0, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '11');
+    await time.advanceBlock();
+    await farming.withdraw(0, 1000, {from: alice});
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '0');
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '2');
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+  });
+
+  it("deposit 0 with nft,lock-time,dual farming", async()=>{
+    await farming.add(100, lp1.address, true, 0, true);
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+    await farming.deposit(0, 0, 3600*24*30, 2, {from: alice});
+  });
+
+  it("deposit amount with nft,lock-time,dual farming", async ()=>{
+    await farming.add(100, lp1.address, true, 0, true);
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+    await farming.deposit(0, 1000, 3600*24*30, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '12');
+    ret = await farming.pendingWasp(0, alice);
+    assert.strictEqual(ret.toString(), '5');
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '8999000');
+
+  });
+
+  it("withdraw 0 with nft,lock-time,dual farming", async ()=>{
+    await farming.add(100, lp1.address, true, 0, true);
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+    await farming.deposit(0, 1000, 3600*24*30, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '12');
+    ret = await farming.pendingWasp(0, alice);
+    assert.strictEqual(ret.toString(), '5');
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '8999000');
+    await farming.withdraw(0, 0, {from: alice});
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '0');
+    ret = await farming.pendingWasp(0, alice);
+    assert.strictEqual(ret.toString(), '0');
+    assert.strictEqual((await zoo.balanceOf(alice)).toString(), '1000024');
+    assert.strictEqual((await wasp.balanceOf(alice)).toString(), '10');
+  });
+
+  it("withdraw amount with nft,lock-time,dual farming", async ()=>{
+    await farming.add(100, lp1.address, true, 0, true);
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+    await farming.deposit(0, 1000, 3, 2, {from: alice});
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '1');
+    await time.advanceBlock();
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '11');
+    ret = await farming.pendingWasp(0, alice);
+    assert.strictEqual(ret.toString(), '5');
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '8999000');
+    await farming.withdraw(0, 0, {from: alice});
+    ret = await farming.pendingZoo(0, alice);
+    assert.strictEqual(ret.toString(), '0');
+    ret = await farming.pendingWasp(0, alice);
+    assert.strictEqual(ret.toString(), '0');
+    assert.strictEqual((await zoo.balanceOf(alice)).toString(), '1000022');
+    assert.strictEqual((await wasp.balanceOf(alice)).toString(), '10');
+    await sleep(5000);
+    await farming.withdraw(0, 1000, {from: alice});
+    assert.strictEqual((await lp1.balanceOf(alice)).toString(), '9000000');
+    assert.strictEqual((await nft.balanceOf(alice)).toString(), '2');
+    assert.strictEqual((await zoo.balanceOf(alice)).toString(), '1000033');
+    assert.strictEqual((await wasp.balanceOf(alice)).toString(), '15');
+
+  });
+
 });

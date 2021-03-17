3fbb2f58a41b9e42cf07e9bdb8c987089186d2bd
lolieatapple <lolieatapple@gmail.com>
2021-03-17 12:01:52 +0800

    update nft test

--- test/nft.test.js
+++ test/nft.test.js
@@ -1,18 +1,18 @@
-const ZooNFTDelegate = artifacts.require('ZooNFTDelegate');
+const ZooNFT = artifacts.require('ZooNFT');
 
 const assert = require('assert');
 
-contract("ZooNFTDelegate", accounts => {
-  let zooNFTDelegate;
+contract("ZooNFT", accounts => {
+  let zooNFT;
   beforeEach(async ()=>{
-    zooNFTDelegate = await ZooNFTDelegate.new();
-    await zooNFTDelegate.initialize(accounts[0]);
+    zooNFT = await ZooNFT.new();
+    await zooNFT.initialize(accounts[0]);
     // config boost
     let pr =[];
     for (let i=1; i<=4; i++) {
       for (let c=1; c<=6; c++) {
         for (let e=1; e<=5; e++) {
-          let ret = await zooNFTDelegate.getLevelChance(i, c, e);
+          let ret = await zooNFT.getLevelChance(i, c, e);
           pr.push(Number(Number(ret.toString())/1e10).toFixed(5));
         }
       }
@@ -33,17 +33,17 @@ contract("ZooNFTDelegate", accounts => {
       reduces.push('0x' + Number((1e10 + i*2e9).toFixed(0)).toString(16));
     }
 
-    await zooNFTDelegate.setBoostMap(chances, boosts, reduces);
+    await zooNFT.setBoostMap(chances, boosts, reduces);
   });
 
   it("should success when set factory", async () => { 
-    await zooNFTDelegate.setNFTFactory(accounts[1]);
+    await zooNFT.setNFTFactory(accounts[1]);
 
   });
 
   it("should failed when set factory without access", async () => {
     try {
-      await zooNFTDelegate.setNFTFactory(accounts[1], {from: accounts[2]});
+      await zooNFT.setNFTFactory(accounts[1], {from: accounts[2]});
 
       assert.fail('never go here');
     } catch (e) {
@@ -52,20 +52,20 @@ contract("ZooNFTDelegate", accounts => {
   });
 
   it("should success when setURI", async () => { 
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
   });
 
   it("should failed when setURI without access", async () => {
     try {
-      await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/', {from: accounts[1]});
+      await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/', {from: accounts[1]});
       assert.fail('never go here');
     } catch (e) {
       assert.ok(e.message.match(/revert/));
     }
 
     try {
-      await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json', {from: accounts[1]});
+      await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json', {from: accounts[1]});
       assert.fail('never go here');
     } catch (e) {
       assert.ok(e.message.match(/revert/));
@@ -73,32 +73,32 @@ contract("ZooNFTDelegate", accounts => {
   });
 
   it("should success when getURI", async () => { 
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
-    let ret = await zooNFTDelegate.getNftURI(1, 1, 1);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    let ret = await zooNFT.getNftURI(1, 1, 1);
     assert.strictEqual(ret, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
   });
 
   it("should empty when getURI without set", async () => {
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
-    let ret = await zooNFTDelegate.getNftURI(3, 1, 1);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    let ret = await zooNFT.getNftURI(3, 1, 1);
     assert.strictEqual(ret, '');
   });
 
   it("should success when mint", async () => { 
-    await zooNFTDelegate.setNFTFactory(accounts[1]);
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
-    await zooNFTDelegate.mint(1, 1, 1, 1, 100, { from: accounts[1] });
+    await zooNFT.setNFTFactory(accounts[1]);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    await zooNFT.mint(1, 1, 1, 1, 100, { from: accounts[1] });
   });
 
   it("should failed when mint without access", async () => {
-    await zooNFTDelegate.setNFTFactory(accounts[1]);
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    await zooNFT.setNFTFactory(accounts[1]);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
     try {
-      await zooNFTDelegate.mint(1, 1, 1, 1, 100, { from: accounts[2] });
+      await zooNFT.mint(1, 1, 1, 1, 100, { from: accounts[2] });
       assert.fail('never go here');
     } catch (e) {
       assert.ok(e.message.match(/revert/));
@@ -106,37 +106,37 @@ contract("ZooNFTDelegate", accounts => {
   });
 
   it("should success when getBoosting", async () => { 
-    await zooNFTDelegate.setNFTFactory(accounts[1]);
-    await zooNFTDelegate.setNFTFactory(accounts[2]);
+    await zooNFT.setNFTFactory(accounts[1]);
+    await zooNFT.setNFTFactory(accounts[2]);
 
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
-    await zooNFTDelegate.mint(1, 1, 1, 1, 100, { from: accounts[1] });
-    ret = await zooNFTDelegate.getBoosting(1);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    await zooNFT.mint(1, 1, 1, 1, 100, { from: accounts[1] });
+    ret = await zooNFT.getBoosting(1);
     assert.strictEqual(ret.toString(), '1011000000000');
-    await zooNFTDelegate.setNftURI(2, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple-pie.json');
-    await zooNFTDelegate.mint(202, 2, 1, 1, 10, { from: accounts[2] });
-    ret = await zooNFTDelegate.getBoosting(202);
+    await zooNFT.setNftURI(2, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple-pie.json');
+    await zooNFT.mint(202, 2, 1, 1, 10, { from: accounts[2] });
+    ret = await zooNFT.getBoosting(202);
     assert.strictEqual(ret.toString(), '1060100000000');
   });
 
   it("should 1e12 when getBoosting non-token", async () => {
-    ret = await zooNFTDelegate.getBoosting(1);
+    ret = await zooNFT.getBoosting(1);
     assert.strictEqual(ret.toString(), '1000000000000');
   });
 
   it("should success when getTokenURI", async () => {
-    await zooNFTDelegate.setNFTFactory(accounts[1]);
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
-    await zooNFTDelegate.mint(1, 1, 1, 1, 100, { from: accounts[1] });
-    ret = await zooNFTDelegate.tokenURI(1);
+    await zooNFT.setNFTFactory(accounts[1]);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    await zooNFT.mint(1, 1, 1, 1, 100, { from: accounts[1] });
+    ret = await zooNFT.tokenURI(1);
     assert.strictEqual(ret, 'https://gateway.pinata.cloud/ipfs/QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
    });
 
   it("should failed when getTokenURI non token", async () => {
     try {
-      ret = await zooNFTDelegate.tokenURI(1);
+      ret = await zooNFT.tokenURI(1);
       assert.fail('never go here');
     } catch (e) {
       assert.ok(e.message.match(/revert/));
@@ -144,11 +144,11 @@ contract("ZooNFTDelegate", accounts => {
   });
 
   it("should success when getTokenInfo", async () => { 
-    await zooNFTDelegate.setNFTFactory(accounts[1]);
-    await zooNFTDelegate.setBaseURI('https://gateway.pinata.cloud/ipfs/');
-    await zooNFTDelegate.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
-    await zooNFTDelegate.mint(1, 1, 1, 1, 100, { from: accounts[1] });
-    ret = await zooNFTDelegate.tokenInfo(1);
+    await zooNFT.setNFTFactory(accounts[1]);
+    await zooNFT.setBaseURI('https://gateway.pinata.cloud/ipfs/');
+    await zooNFT.setNftURI(1, 1, 1, 'QmZ7ddzc9ZFF4dsZxfYhu26Hp3bh1Pq2koxYWkBY6vbeoN/apple.json');
+    await zooNFT.mint(1, 1, 1, 1, 100, { from: accounts[1] });
+    ret = await zooNFT.tokenInfo(1);
     assert.strictEqual(ret.level.toString(), '1');
     assert.strictEqual(ret.category.toString(), '1');
     assert.strictEqual(ret.item.toString(), '1');
@@ -156,7 +156,7 @@ contract("ZooNFTDelegate", accounts => {
   });
 
   it("should 0 when getTokenInfo non token", async () => {
-    ret = await zooNFTDelegate.tokenInfo(1);
+    ret = await zooNFT.tokenInfo(1);
     assert.strictEqual(ret.level.toString(), '0');
     assert.strictEqual(ret.category.toString(), '0');
     assert.strictEqual(ret.item.toString(), '0');
@@ -164,7 +164,7 @@ contract("ZooNFTDelegate", accounts => {
   });
 
   it("should success when setMultiNftURI", async () => {
-    await zooNFTDelegate.setMultiNftURI(
+    await zooNFT.setMultiNftURI(
       [1, 2],
       [1, 1],
       [1, 1],
@@ -176,7 +176,7 @@ contract("ZooNFTDelegate", accounts => {
 
   it("should failed when setMultiNftURI without access", async () => {
     try {
-      await zooNFTDelegate.setMultiNftURI(
+      await zooNFT.setMultiNftURI(
         [1, 2],
         [1, 1],
         [1, 1],

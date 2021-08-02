1b370a41169a777fc7f5b501ad6498b584412c8d
lolieatapple <lolieatapple@gmail.com>
2021-08-02 22:58:03 +0800

    update zoorena deploy

--- migrations/4_zoorena_migration.js
+++ migrations/4_zoorena_migration.js
@@ -9,18 +9,18 @@ module.exports = async function (deployer) {
   }
 
   // TODO: DEBUG
-  await deployer.deploy(ZoorenaDelegate);
-  return;
+  // await deployer.deploy(ZoorenaDelegate);
+  // return;
 
   let deployerAddr = deployer.provider.addresses[0];
   console.log('deployerAddr', deployerAddr);
-  //TODO: TESTNET CONFIG----------
-  let proxyAdmin = '0x5560aF0F46D00FCeA88627a9DF7A4798b1b10961';
-  let admin = '0x4Cf0A877E906DEaD748A41aE7DA8c220E4247D9e';
-  let robot = '0x4Cf0A877E906DEaD748A41aE7DA8c220E4247D9e';
-  let zooToken = '0x890589dC8BD3F973dcAFcB02b6e1A133A76C8135';
-  let nftFactory = '0x40B4653a2263c9A6634018365Fa5aa5a81E0b0Bd';
-  let zooNFT = '0xbCF9F4fae90dA7c4BB05DA6f9E9A9A39dc5Ce979';
+  //TODO: MAINNET CONFIG----------
+  let proxyAdmin = '0xa206e4858849f70c3d684e854e7C126EF7baB32e';
+  let admin = '0x83f83439Cc3274714A7dad32898d55D17f7C6611';
+  let robot = '0x840e14f597627b4d7aa77bc4001e2d0318c5bd7c';
+  let zooToken = '0x6e11655d6aB3781C6613db8CB1Bc3deE9a7e111F';
+  let nftFactory = '0xBCE166860F514b6134AbC6E9Aa0005CC489b6352';
+  let zooNFT = '0x38034B2E6ae3fB7FEC5D895a9Ff3474bA0c283F6';
   let posRandom = '0x0000000000000000000000000000000000000262';
   //--------------------
 
@@ -36,16 +36,20 @@ module.exports = async function (deployer) {
   await zoorena.configRobot(robot);
 
   await zoorena.configEventOptions(0, 2);
-  await zoorena.configEventOptions(1, 4);
-  await zoorena.configEventOptions(2, 8);
+  await zoorena.configEventOptions(1, 2);
+  await zoorena.configEventOptions(2, 3);
   await zoorena.configEventOptions(3, 4);
-  await zoorena.configEventOptions(4, 2);
-  await zoorena.configEventOptions(5, 4);
-  await zoorena.configEventOptions(6, 8);
-  await zoorena.configEventOptions(7, 10);
-  await zoorena.configEventOptions(8, 3);
+  await zoorena.configEventOptions(4, 4);
+  await zoorena.configEventOptions(5, 6);
+  await zoorena.configEventOptions(6, 6);
+  await zoorena.configEventOptions(7, 8);
+  await zoorena.configEventOptions(8, 10);
+  await zoorena.configEventOptions(9, 12);
 
-  await zoorena.configTime(1622851200, 24*3600, 15*3600);
+  // baseTime: UTC 2021-8-2 00:00:00
+  // round time: 7 days
+  // close time: every Sunday UTC 14:00
+  await zoorena.configTime(1627862400, 7*24*3600, (14+24*6)*3600);
 
   await zoorena.grantRole('0x00', admin);
 

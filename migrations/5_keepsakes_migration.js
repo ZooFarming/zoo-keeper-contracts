553da5fb0296ea005ec5b188fef5ba881b47ade2
lolieatapple <lolieatapple@gmail.com>
2021-08-17 23:44:38 +0800

    update keepsakes migration

---
+++ migrations/5_keepsakes_migration.js
@@ -0,0 +1,49 @@
+// const Migrations = artifacts.require("Migrations");
+const KeepsakesNFT = artifacts.require("KeepsakesNFT");
+const KeepsakesCreatorDelegate = artifacts.require("KeepsakesCreatorDelegate");
+const ZooKeeperProxy = artifacts.require('ZooKeeperProxy');
+
+module.exports = async function (deployer) {
+  if (deployer.network === 'development' || deployer.network === 'coverage') {
+    console.log('no need migration');
+    return;
+  }
+
+  let deployerAddr = deployer.provider.addresses[0];
+  console.log('deployerAddr', deployerAddr);
+  //TODO: TESTNET CONFIG----------
+  let proxyAdmin = '0x5560aF0F46D00FCeA88627a9DF7A4798b1b10961';
+  let admin = '0x4Cf0A877E906DEaD748A41aE7DA8c220E4247D9e';
+  //--------------------
+
+  await deployer.deploy(KeepsakesNFT);
+
+  let nft = await KeepsakesNFT.deployed();
+
+  await nft.initialize(admin);
+
+  await deployer.deploy(KeepsakesCreatorDelegate);
+
+  let creatorDelegate = await KeepsakesCreatorDelegate.deployed();
+
+  await deployer.deploy(ZooKeeperProxy, creatorDelegate.address, proxyAdmin, '0x');
+
+  let creator = await KeepsakesCreatorDelegate.at((await ZooKeeperProxy.deployed()).address);
+  
+  await creator.initialize(deployerAddr, nft.address);
+
+  await nft.setNFTFactory(creator.address);
+
+  // TODO:
+  await creator.addAuthor(admin);
+
+  await creator.grantRole('0x00', admin);
+
+  if (deployerAddr.toLowerCase() !== admin.toLowerCase()) {
+    console.log('renounceRole:', deployerAddr);
+    await creator.renounceRole('0x00', deployerAddr);
+  }
+
+  console.log('keepsakes creator:', creator.address);
+
+}

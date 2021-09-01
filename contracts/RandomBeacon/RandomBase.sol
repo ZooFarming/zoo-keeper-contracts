954c142f94beb451c1ad874ff7a1ae8df95d3f7a
lolieatapple <lolieatapple@gmail.com>
2021-09-01 21:27:00 +0800

    update

---
+++ contracts/RandomBeacon/RandomBase.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+contract RandomBase {
+  event RequestRandom(address indexed callback, uint indexed id);
+
+  function requestRandom(address callback, uint id) internal {
+
+  }
+}

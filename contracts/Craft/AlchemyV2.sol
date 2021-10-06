0b2aa40673a3a1ade523bb89bc363c7a492ea255
lolieatapple <lolieatapple@gmail.com>
2021-10-06 21:42:33 +0800

    feat: add elixir transfer

--- contracts/Craft/AlchemyV2.sol
+++ contracts/Craft/AlchemyV2.sol
@@ -25,4 +25,28 @@ contract AlchemyV2 is AlchemyV1 {
 
         super.buy(IRandomElixirName(randomNameAddr).generateName(randomSeed));
     }
+
+    function getTransferFee(uint tokenId) public view returns (uint) {
+        uint price = getElixirPrice();
+        uint level = elixirInfoMap[tokenId].level;
+        // price * 0.1 + price * 0.1 * level / 10
+        return price.div(10).add(price.div(10).mul(level).div(10));
+    }
+
+    function transferElixir(uint tokenId, address to) external {
+        uint fee = getTransferFee(tokenId);
+
+        IERC20(buyToken).safeTransferFrom(
+            msg.sender,
+            address(this),
+            fee
+        );
+        IBurnToken(buyToken).burn(fee);
+
+        IERC721(elixirNFT).safeTransferFrom(
+            msg.sender,
+            to,
+            tokenId
+        );
+    }
 }
